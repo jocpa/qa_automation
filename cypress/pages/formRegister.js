29ef033f66e7043bfb2e5a569620db55852c9034
Pablo Arzu <jptomas38@gmail.com>
2023-06-14 10:37:38 -0600

    formRegister.js: metodos creados

---
+++ cypress/pages/formRegister.js
@@ -0,0 +1,44 @@
+const { last } = require("cypress/types/lodash");
+
+class formRegister {
+
+    elements = {
+
+        firstName: () => cy.get('#firstName'),
+        lastName: () => cy.get('#lastName'),
+        phoneNumber: () => cy.get('#phone'),
+        country: () => cy.get('#countries_dropdown_menu'),
+        email: () => cy.get('#emailAddress'),
+        password: () => cy.get('#password'),
+        termns: () => cy.get('#exampleCheck1'),
+        registerBtn: () => cy.get('#registerBtn')
+
+    }
+
+    typeFirstName(name) {
+        this.elements.firstName().type(name);
+    }
+    typeLastName(lastName) {
+        this.elements.lastName().type(lastName)
+    }
+    typePhoneNumber(phone) {
+        this.elements.phoneNumber().type(phone)
+    }
+    selectCountry(country) {
+        this.elements.country().select(country)
+    }
+    typeEmail(email) {
+        this.elements.email().type(email)
+    }
+    typePassword(pass) {
+        this.elements.password().type(pass)
+    }
+    termsClick() {
+        this.elements.termns().click()
+    }
+    clickRegister() {
+        this.elements.registerBtn().click()
+    }
+
+}
+module.exports = new formRegister();
